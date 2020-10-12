5ca2883d1769605a4524c9ed73845e2cdd858f98
ritikgarg655 <18k009@nirmauni.ac.in>
2020-10-12 23:14:44 +0530

    Added index function in searchnofor.pyy

--- searchnofor.py
+++ searchnofor.py
@@ -1,97 +1,69 @@
-
-from typing import Tuple
-
-
 class TrieNode(object):
-    """
-    Our trie node implementation. Very basic. but does the job
-    """
-    
     def __init__(self, char: str):
         self.char = char
         self.children = [0]*26
-        # Is it the last character of the word.`
         self.word_finished = False
-        # How many times this character appeared in the addition process
-        self.counter = 1
-    
+        self.end_word = False
+        self.end_index = -1
 
-def add(root, word: str):
-    """
-    Adding a word in the trie structure
-    """
+def add(root, word: str,index):
     node = root
-    for char in word:
+    for i in range(0,len(word)):
+        char = word[i]
         found_in_child = False
         # Search for the character in the children of the present `node`
         if node.children[ord(char)-ord('a')] != 0:
-                # We found it, increase the counter by 1 to keep track that another
-                # word has it as well
-            # node.children.counter += 1
-                # And point the node to the child that contains this char
             node = node.children[ord(char)-ord('a')]
             found_in_child = True
-            break
-        # We did not find it so add a new chlid
+            if i == len(word)-1:
+                # add end word here
+                node.end_word = True
+                node.index = index
         if not found_in_child:
             new_node = TrieNode(char)
             node.children[ord(char)-ord('a')] = new_node
-            # And then point node to the new child
             node = new_node
-    # Everything finished. Mark it as the end of a word.
+            if i == len(word)-1:
+                # add end word here
+                node.end_word = True
+                node.index = index
+
     node.word_finished = True
 
+def traverse(root,list_index):
+    if root == 0:
+        return
+    for i in range(0,26):
+        traverse(root.children[i],list_index)
+    if root.end_word:
+        list_index.append(root.index)
 
-def find_prefix(root, prefix: str) -> Tuple[bool, int]:
-    """
-    Check and return 
-      1. If the prefix exsists in any of the words we added so far
-      2. If yes then how may words actually have the prefix
-    """
+def find_prefix(root, prefix: str):
     node = root
-    # If the root node has no children, then return False.
-    # Because it means we are trying to search in an empty trie
-    if not root.children:
-        return False, 0
-
     for char in prefix:
         char_not_found = True
-        # Search through all the children of the present `node`
         if node.children[ord(char)-ord('a')] != 0:
-                # We found it, increase the counter by 1 to keep track that another
-                # word has it as well
-            
-                # And point the node to the child that contains this char
             node = node.children[ord(char)-ord('a')]
             char_not_found = False
-            # break
-        # Return False anyway when we did not find a char.
         if char_not_found:
-            return False
-    # Well, we are here means we have found the prefix. Return true to indicate that
-    # And also the counter of the last node. This indicates how many words have this
-    # prefix
-    return True
+            return []
+    list_index = []
+    traverse(node,list_index)
+    return list_index
 
 if __name__ == "__main__":
     root = TrieNode('*')
     sentance=input("Add something ")
     words=sentance.split()
     for w in words:
-        add(root,w)
+        add(root,w,2)
     
     sen=input("Search something ")
     wo=sen.split()
     for w in wo:
         print(find_prefix(root,w))
-    
-    
-        
-    
-    
-    
-    add(root, "hackathon")
-    add(root, 'hack')
+    add(root, "hackathon",1)
+    add(root, 'hack',0)
 
     print(find_prefix(root, 'hac'))
     print(find_prefix(root, 'hack'))
