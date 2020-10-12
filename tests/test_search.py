b353a112026309b72a945b71e525f5321889c85b
ritikgarg655 <18k009@nirmauni.ac.in>
2020-10-13 00:34:04 +0530

    Added test file for search.py

--- tests/test_search.py
+++ tests/test_search.py
@@ -4,3 +4,9 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..', ''))
 from search import *
 
 add_dic(data)
+
+while 1:
+	query_q = input("Enter query")
+	lis = query(query_q)
+	for i in lis:
+		print(data[i])
