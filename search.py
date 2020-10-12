494271da9d9feef25079741444bfbc6b1a7ae41f
ritikgarg655 <18k009@nirmauni.ac.in>
2020-10-13 00:14:30 +0530

    Added test file for search.py

--- search.py
+++ search.py
@@ -1,99 +1,78 @@
+from index_intersection import *
 
-from typing import Tuple
-
-
+def return_index( w):
+    if(w<='9' and w>='0'):
+        return ord(w)-ord('0')
+    else:
+        return ord(w)-ord('a')+10
 class TrieNode(object):
-    """
-    Our trie node implementation. Very basic. but does the job
-    """
-    
     def __init__(self, char: str):
         self.char = char
-        self.children = []
-        # Is it the last character of the word.`
+        self.children = [0]*36
         self.word_finished = False
-        # How many times this character appeared in the addition process
-        self.counter = 1
-    
+        self.end_word = False
+        self.end_index = -1
 
-def add(root, word: str):
-    """
-    Adding a word in the trie structure
-    """
+def add(root, word: str,index):
     node = root
-    for char in word:
+    for i in range(0,len(word)):
+        char = word[i]
         found_in_child = False
         # Search for the character in the children of the present `node`
-        for child in node.children:
-            if child.char == char:
-                # We found it, increase the counter by 1 to keep track that another
-                # word has it as well
-                child.counter += 1
-                # And point the node to the child that contains this char
-                node = child
-                found_in_child = True
-                break
-        # We did not find it so add a new chlid
+        if node.children[return_index(char)] != 0:
+            node = node.children[return_index(char)]
+            found_in_child = True
+            if i == len(word)-1:
+                # add end word here
+                node.end_word = True
+                node.index = index
         if not found_in_child:
             new_node = TrieNode(char)
-            node.children.append(new_node)
-            # And then point node to the new child
+            node.children[return_index(char)] = new_node
             node = new_node
-    # Everything finished. Mark it as the end of a word.
+            if i == len(word)-1:
+                # add end word here
+                node.end_word = True
+                node.index = index
+
     node.word_finished = True
 
+def traverse(root,list_index):
+    if root == 0:
+        return
+    for i in range(0,36):
+        traverse(root.children[i],list_index)
+    if root.end_word:
+        list_index.append(root.index)
 
-def find_prefix(root, prefix: str) -> Tuple[bool, int]:
-    """
-    Check and return 
-      1. If the prefix exsists in any of the words we added so far
-      2. If yes then how may words actually have the prefix
-    """
+def find(root, prefix: str):
     node = root
-    # If the root node has no children, then return False.
-    # Because it means we are trying to search in an empty trie
-    if not root.children:
-        return False, 0
     for char in prefix:
         char_not_found = True
-        # Search through all the children of the present `node`
-        for child in node.children:
-            if child.char == char:
-                # We found the char existing in the child.
-                char_not_found = False
-                # Assign node as the child containing the char and break
-                node = child
-                break
-        # Return False anyway when we did not find a char.
+        if node.children[return_index(char)] != 0:
+            node = node.children[return_index(char)]
+            char_not_found = False
         if char_not_found:
-            return False, 0
-    # Well, we are here means we have found the prefix. Return true to indicate that
-    # And also the counter of the last node. This indicates how many words have this
-    # prefix
-    return True, node.counter
+            return []
+    list_index = []
+    traverse(node,list_index)
+    return list_index
 
-if __name__ == "__main__":
-    root = TrieNode('*')
-    sentance=input("Add something ")
-    words=sentance.split()
-    for w in words:
-        add(root,w)
-    
-    sen=input("Search something ")
-    wo=sen.split()
-    for w in wo:
-        print(find_prefix(root,w))
-    
-    
-        
-    
-    
-    
-#     add(root, "hackathon")
-#     add(root, 'hack')
+def insert(root,word,index):
+    for i in range(0,len(word)):
+        for j in range(i+1,len(word)):
+            add(root,word[i:j+1],index)
 
-#     print(find_prefix(root, 'hac'))
-#     print(find_prefix(root, 'hack'))
-#     print(find_prefix(root, 'hackathon'))
-#     print(find_prefix(root, 'ha'))
-#     print(find_prefix(root, 'hammer'))
+def add_dic(list_sentence):
+    
+    for index in range(0,len(list_sentence)):
+        for word in list_sentence[index].split():
+            insert(root,word,index)
+
+def query(que):
+    combine_list = []
+    for word in que.split():
+        combine_list.append(find(root,word))
+    return index_intersection(combine_list)
+
+root = TrieNode('*')
---
+++ tests/test_search.py
@@ -0,0 +1,6 @@
+from test_data import *
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', ''))
+from search import *
+
+add_dic(data)
