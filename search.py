31775376c678339b8959e8717732538a2a49ae41
sarthak20535 <56092219+sarthak20535@users.noreply.github.com>
2020-10-13 18:12:09 +0530

    Update search.py

--- search.py
+++ search.py
@@ -18,6 +18,20 @@ def similar_words(s):
             synonyms.append(lemma.name())
     return synonyms
 
+def stop_filter(temp):
+    import nltk
+    from nltk.corpus import stopwords 
+    from nltk.tokenize import word_tokenize 
+    
+    stop_words = set(stopwords.words('english'))
+    word_tokens = word_tokenize(temp) 
+    
+    filtered_sentence=[]
+    for w in word_tokens: 
+        if w not in stop_words: 
+            filtered_sentence.append(w) 
+    return filtered_sentence
+
 def return_index( w):
     l = [',','.','/','~','!','#','\'','$','%','&','(',')','…',':','-']
     if(w<='9' and w>='0'):
