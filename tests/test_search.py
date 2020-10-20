6b160cfac99e03ab65285b5338a01b9c3c55cecc
ritikgarg655 <18k009@nirmauni.ac.in>
2020-10-20 15:57:35 +0530

    final all done, all bug removed, working fine.

--- tests/test_search.py
+++ tests/test_search.py
@@ -2,8 +2,13 @@ from test_data import *
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', ''))
 from search import *
-
-add_dic(data)
+filters = {
+    "lower":True,
+    "synonyms":False,
+    "split":True,
+	"stopword":False
+}
+add_dic(data,filters)
 
 while 1:
 	query_q = input("Enter query")
